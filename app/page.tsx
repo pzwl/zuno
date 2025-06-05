f587909bdcdc5e32f29c7108780febaf5f46f4de
pzwl <sharmaprajjwal333@gmmail.com>
2025-06-05 18:58:17 +0530

    fixing deployment issue , dynamic shit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import CTA from "@/components/CTA";
 import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
 import {getSubjectColor} from "@/lib/utils";
 
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
+
     const companions = await getAllCompanions({ limit: 3 });
     const recentSessionsCompanions = await getRecentSessions(10);
 
